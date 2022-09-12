08bdb0618a556593171865d140f1a726d87409ac
qdzhang <qdzhangcn@gmail.com>
2022-09-12 19:01:34 +0800

    Make linter happy

--- quick-insert-unicode.el
+++ quick-insert-unicode.el
@@ -1,12 +1,18 @@
-;;; quick-insert-unicode.el --- Quick insert unicode characters. -*- lexical-binding: t -*-
+;;; quick-insert-unicode.el --- Quick insert unicode characters  -*- lexical-binding: t; -*-
 
-;; Copyright © 2022 qdzhang
-;; Copyright © 2015 Emanuel Evans
+;; Copyright (C) 2022  qdzhang
+;; Copyright (C) 2015 Emanuel Evans
 
+;; Author: qdzhang <qdzhangcn@gmail.com>
+;; Maintainer: qdzhang <qdzhangcn@gmail.com>
+;; URL: https://github.com/qdzhang/quick-insert-unicode
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: insert unicode
 
-;; This program is free software; you can redistribute it and/or modify
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
@@ -17,15 +23,17 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
+;;
 ;; quick-insert-unicode
 ;; ====================
 
 ;;   A completing-read command for looking up unicode characters by name
-;;   😉.  Modified from <https://github.com/qdzhang/ivy-unicode>.
+;;   😉.  Modified from <https://github.com/qdzhang/ivy-unicode> and
+;;   <https://github.com/bomgar/helm-unicode>.
+
 
 ;; Installation
 ;; ~~~~~~~~~~~~
@@ -41,9 +49,10 @@
 ;; ~~~~~
 
 ;;   `quick-insert-unicode'
-;;     The main function to use, bind it to a key whatever you like. This
+;;     The main function to use, bind it to a key whatever you like.  This
 ;;     function use `completing-read' to select unicode characters, and
 ;;     insert the selected item to current posotion.
+;;
 
 ;;; Code:
 
@@ -52,7 +61,7 @@
 Should not be changed by the user.")
 
 (defun quick-insert-unicode-format-char-pair (char-pair)
-  "Formats a char pair for quick-insert unicode search."
+  "Formats a CHAR-PAIR for quick-insert unicode search."
   (let ((name (car char-pair))
         (symbol (cdr char-pair)))
     (format "%s %c" name symbol)))
@@ -72,10 +81,10 @@ Should not be changed by the user.")
 
 ;;;###autoload
 (defun quick-insert-unicode (arg)
-  "Use `completing-read' for looking up unicode characters by name, and insert
-the selected item.
+  "Quick insert unicode characters.
 
-With prefix ARG, reinitialize the cache."
+Use `completing-read' for looking up unicode characters by name, and insert the
+selected item. With prefix ARG, reinitialize the cache."
   (interactive "P")
   (when arg (setq quick-insert-unicode-names nil))
   ((lambda (x) (quick-insert-unicode-insert-char x))
